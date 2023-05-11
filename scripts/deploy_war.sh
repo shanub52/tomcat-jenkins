273b94894fa56d3b51123ca5b4862533b0e9ea78
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-11 15:43:52 +0530

    update build file 1

--- scripts/deploy_war.sh
+++ scripts/deploy_war.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Define the path to the WAR file
-WAR_FILE=/opt/myapp/myapp1.war
+WAR_FILE=/opt/myapp/gs-*.war
 
 # Define the path to the Tomcat installation directory
 TOMCAT_DIR=/opt/tomcat
@@ -11,7 +11,7 @@ TOMCAT_DIR=/opt/tomcat
 sudo systemctl stop tomcat
 
 # Remove the old application files
-sudo rm -rf $TOMCAT_DIR/webapps/myapp*
+sudo rm -rf $TOMCAT_DIR/webapps/gs-*
 
 # Copy the new WAR file to the Tomcat webapps directory
 sudo cp $WAR_FILE $TOMCAT_DIR/webapps/
