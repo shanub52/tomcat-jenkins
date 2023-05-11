9c2c40aca7a6aa6e6f595c54efba573afb5c255a
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-11 14:18:26 +0530

    update latest code

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-set -x
-APP_HOME="/home/ec2-user/server2"
-cd $APP_HOME
-sudo /usr/bin/java -jar *.jar >/var/tmp/app.log 2>>/var/tmp/app.log < /dev/null &
+systemctl start tomcat
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-set -x
-APP_HOME="/home/ec2-user/server2"
-ifAppRunning=`pgrep java`
-if [[ -n  $ifAppRunning ]]; then
-   sudo killall java 
-fi
-echo "listing files..."
-ls -l $APP_HOME/
+systemctl stop tomcat
